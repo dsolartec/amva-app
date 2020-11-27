cf0e24bd9638089d85e17c6e7db4d57698a14bfc
Daniel Solarte <danielsolartech@hotmail.com>
2020-11-27 10:35:28 -0500

    feat(WEB): Add player animation

--- web/src/components/Player.js
+++ web/src/components/Player.js
@@ -1,31 +1,42 @@
 import React from 'react';
+import { Spring } from 'react-spring/renderprops';
 import './Player.scss';
 
 // Videos
 import celeste_lowres from '../assets/videos/celeste4_3-lowres.mp4';
 
-export default function Player({ size, active, onVideoEnded }) {
+export default function Player({ active, onVideoEnded }) {
+    const [isShowing, setShowing] = React.useState(true);
     const videoSource = React.useRef();
 
     if (!active) {
         return null;
     }
 
+    const fromPosition = isShowing ? '-40rem' : '1rem';
+    const toPosition = isShowing ? '1rem' : '-40rem';
+
     return (
-        <div className="player">
-            <div className="box">
-                <video
-                    src={celeste_lowres}
-                    ref={videoSource}
-                    autoPlay={true}
-                    onEnded={onVideoEnded}
-                    onClick={() => {
-                        if (videoSource.current) {
-                            videoSource.current[videoSource.current.paused ? "play" : "pause"]();
-                        }
-                    }}
-                />
-            </div>
-        </div>
+        <Spring
+            from={{ right: fromPosition, top: fromPosition }}
+            to={{ right: toPosition, top: toPosition }}
+            onRest={() => onVideoEnded}
+        >
+            {props => (
+                <div className="player" style={props}>
+                    <video
+                        src={celeste_lowres}
+                        ref={videoSource}
+                        autoPlay={true}
+                        onEnded={() => setShowing(false)}
+                        onClick={() => {
+                            if (videoSource.current) {
+                                videoSource.current[videoSource.current.paused ? "play" : "pause"]();
+                            }
+                        }}
+                    />
+                </div>
+            )}
+        </Spring>
     );
 }
--- web/src/states.js
+++ web/src/states.js
@@ -14,20 +14,6 @@ export const INITIAL_STATE = {
 
 const transitionDuration = 2500;
 
-export const AMVA_STATE = {
-    view_state: {
-        longitude: -75.552464,
-        latitude: 6.326047,
-        zoom: 10.5,
-        bearing: -60,
-        pitch: 60,
-        transitionDuration,
-        transitionInterpolator: new FlyToInterpolator(),
-    },
-    layers: ['layer_dem', 'layer_amva_arcs'],
-    map_style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
-};
-
 export const AMVA_STATE_180deg = {
     view_state: {
         longitude: -75.552464,
