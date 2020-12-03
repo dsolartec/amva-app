12e716989dfab2508b225a6fe78dde332d57f4e3
brolin <brolin108@gmail.com>
2020-12-03 11:22:41 -0500

    Fix: Footer texts to achieve the AMVA design standard

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -3,12 +3,12 @@ import Countdown from './Countdown';
 import './Footer.scss';
 
 const texts = {
-    amva: 'Con una línea que nos trazamos, acercamos territorios, culturas, diversidades, puntos de vista, sueños y personas. Cada punto es un foco de gestión que se une a través de la tecnología, la movilidad, el desarrollo y la conservación ambiental para articular un Futuro Sostenible que no se detiene y crea calidad de vida.',
-    capsule: 'Las urnas del tiempo clásicas querían mostrar a generaciones futuras cómo era la vida años atrás; la Cápsula "Observatorio de lo invisible" viene del pasado y el futuro para decirnos que la llave del acertijo la tenemos hoy en nuestras manos.',
+    amva: 'Con una línea que nos trazamos, acercamos territorios, culturas, diversidades, puntos <br/> de vista, sueños y personas. Cada punto es un foco de gestión que se une a través <br/> de la tecnología, la movilidad, el desarrollo y la conservación ambiental para articular un Futuro Sostenible que no se detiene y crea calidad de vida.',
+    capsule: 'Las urnas del tiempo clásicas querían mostrar a generaciones futuras cómo era la vida años atrás; la "Cápsula Viva Observatorio de lo Invisible" viene del pasado y el futuro para decirnos que la llave del acertijo la tenemos hoy en nuestras manos.',
     video: `“Tendemos a ignorar las cosas que son invisibles, ¿estás de acuerdo? 
     ¿Invisibles? 
-    El clima es invisible y, sin embargo, nos afecta.” 
-    “Pero la mejor manera de entender el silencio consiste en aceptar que no  
+    <br/> El clima es invisible y, sin embargo, nos afecta”, 
+    “Pero la mejor manera de entender <br/> el silencio consiste en aceptar que no  
     existe, sino que es una medida a partir de la cual valoramos el sonido.” 
     Opus zero, Daniel G`
 };
@@ -29,7 +29,7 @@ export default function Footer({ view }) {
 
     return (
         <div className="footer">
-            <div className="text">{texts[view]}</div>
+            <div className="text" dangerouslySetInnerHTML={{ __html: texts[view] }}/>
             <Countdown />
         </div>
     );
